1dfd05cdf58b25354ba3507e68935131d3e766b5
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2020-05-10 21:42:56 +0530

    npm publish 1.0.1

--- projects/route-guards/src/public-api.ts
+++ projects/route-guards/src/public-api.ts
@@ -2,5 +2,4 @@
  * Public API Surface of route-guards
  */
 
-export * from './lib/angular-fire-auth-guard/angular-fire-auth-guard.module';
 export * from './lib/angular-fire-auth-guard/angular-fire-auth.guard';
